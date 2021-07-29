9c4baa7e10c1fc0aad859010f57757682ad988a7
jonas060831 <jonas_sulit2001@yahoo.com>
2021-07-29 09:19:43 -0700

    testing environment variables

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+const creator = process.env.NEXT_PUBLIC_CREATOR_NAME
+
 const Home = () => {
   return (
     <div className={styles.container}>
-      <h1>hello 123</h1>
+      <h1>hello {creator} </h1>
     </div>
   )
 }
+
 
 export default Home
